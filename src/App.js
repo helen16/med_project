7b1b17dba71fb9e5c4388b96f993febdb582c212
helen16 <keep.in.mind32@gmail.com>
2024-02-12 05:27:54 -0500

    Dynamic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing_Page from './Components/Landing_Page/Landing_Page';
-
- 
-
-
 import Navbar from './Components/Navbar/Navbar';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
+
+
 function App() {
   return (
     <div className="App">
@@ -13,6 +13,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path='/' element={<Landing_Page/>}></Route>
+              <Route path='/Sign_Up' element={<Sign_Up/>}></Route>
+              <Route path='/Login' element={<Login/>}></Route>
 
               </Routes>
             
