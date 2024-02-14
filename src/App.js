aaa55e9faca37490a7f059dc2ddca41c549a8a01
helen16 <keep.in.mind32@gmail.com>
2024-02-14 10:50:49 -0500

    week4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './Components/Navbar/Navbar';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';  
+import Notification from './Components/Notification/Notification';
+
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
               <Route path='/Sign_Up' element={<Sign_Up/>}></Route>
               <Route path='/Login' element={<Login/>}></Route>
               <Route path="/instant-consultation" element={<InstantConsultation />} />
+              <Route path='/Notification' element={<Notification/>}></Route>
+
               </Routes>
             
         </BrowserRouter>
